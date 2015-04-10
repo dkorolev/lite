184d697e7ab00f5e887de0d3f588b756170eff90
Dima <dmitry.korolev@gmail.com>
2015-04-10 13:32:55 -0700

    Further simplifications.

--- ios/SampleClient/main.m
+++ ios/SampleClient/main.m
@@ -29,7 +29,7 @@
 
 int main(int argc, char * argv[]) {
   @autoreleasepool {
-      int result = UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+      const int result = UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
       [AlohalyticsLite logEvent:@"$leavingMain"];
       return result;
   }
