75bd600e245b992cef5835e32f0b10c461db25a1
Dima <dmitry.korolev@gmail.com>
2015-04-16 02:01:30 -0700

    clang-format

--- ios/SampleClient/Current/Midichlorians.h
+++ ios/SampleClient/Current/Midichlorians.h
@@ -1,21 +1,21 @@
 /*******************************************************************************
  The MIT License (MIT)
- 
+
  Copyright (c) 2015:
- 
+
  * Dmitry "Dima" Korolev <dmitry.korolev@gmail.com>
  * Alexander Zolotarev <me@alex.bio> from Minsk, Belarus
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,7 +26,8 @@
  *******************************************************************************/
 
 #ifndef __cplusplus
-#error "This file is a C++ header, and it should be `#include`-d or `#import`-ed from an `.mm`, not an `.m` source file."
+#error \
+    "This file is a C++ header, and it should be `#include`-d or `#import`-ed from an `.mm`, not an `.m` source file."
 #endif
 
 #ifndef CURRENT_MIDICHLORIANS_H
@@ -40,20 +41,18 @@
 #include "../../../Bricks/cerealize/cerealize.h"
 
 struct MidichloriansEvent {
-    mutable std::string device_id;
-    void SetDeviceId(const std::string& did) const {
-        device_id = did;
-    }
-    
-    template <class A>
-    void serialize(A& ar) {
-        ar(CEREAL_NVP(device_id));
-    }
-    
-    virtual std::string EventAsString(const std::string& device_id) const {
-        SetDeviceId(device_id);
-        return JSON(WithBaseType<MidichloriansEvent>(*this));
-    }
+  mutable std::string device_id;
+  void SetDeviceId(const std::string& did) const { device_id = did; }
+
+  template <class A>
+  void serialize(A& ar) {
+    ar(CEREAL_NVP(device_id));
+  }
+
+  virtual std::string EventAsString(const std::string& device_id) const {
+    SetDeviceId(device_id);
+    return JSON(WithBaseType<MidichloriansEvent>(*this));
+  }
 };
 
 // A helper macro to define structured events.
@@ -62,71 +61,81 @@ struct MidichloriansEvent {
 #error "The `CURRENT_EVENT` macro should not be defined."
 #endif
 
-#define CURRENT_EVENT(M_EVENT_CLASS_NAME, M_IMMEDIATE_BASE)         \
-struct M_EVENT_CLASS_NAME;                                  \
-CEREAL_REGISTER_TYPE(M_EVENT_CLASS_NAME);                   \
-struct M_EVENT_CLASS_NAME##Helper : M_IMMEDIATE_BASE {    \
-typedef MidichloriansEvent CEREAL_BASE_TYPE;              \
-typedef M_IMMEDIATE_BASE SUPER;                           \
-virtual std::string EventAsString(const std::string& device_id) const override {      \
-SetDeviceId(device_id); \
-return JSON(WithBaseType<MidichloriansEvent>(*this));   \
-}                                                         \
-template <class A>                                        \
-void serialize(A& ar) {                                   \
-SUPER::serialize(ar);                                   \
-}                                                         \
-};                                                          \
-struct M_EVENT_CLASS_NAME : M_EVENT_CLASS_NAME##Helper
+#define CURRENT_EVENT(M_EVENT_CLASS_NAME, M_IMMEDIATE_BASE)                          \
+  struct M_EVENT_CLASS_NAME;                                                         \
+  CEREAL_REGISTER_TYPE(M_EVENT_CLASS_NAME);                                          \
+  struct M_EVENT_CLASS_NAME##Helper : M_IMMEDIATE_BASE {                             \
+    typedef MidichloriansEvent CEREAL_BASE_TYPE;                                     \
+    typedef M_IMMEDIATE_BASE SUPER;                                                  \
+    virtual std::string EventAsString(const std::string& device_id) const override { \
+      SetDeviceId(device_id);                                                        \
+      return JSON(WithBaseType<MidichloriansEvent>(*this));                          \
+    }                                                                                \
+    template <class A>                                                               \
+    void serialize(A& ar) {                                                          \
+      SUPER::serialize(ar);                                                          \
+    }                                                                                \
+  };                                                                                 \
+  struct M_EVENT_CLASS_NAME : M_EVENT_CLASS_NAME##Helper
 
 // Generic iOS events.
 
 CURRENT_EVENT(iOSEvent, MidichloriansEvent) {
-    std::string description;
-    template<typename A> void serialize(A& ar) {
-        SUPER::serialize(ar);
-        ar(CEREAL_NVP(description));
-    }
-    iOSEvent() = default;
-    iOSEvent(const std::string& d) : description(d) {}
+  std::string description;
+  template <typename A>
+  void serialize(A & ar) {
+    SUPER::serialize(ar);
+    ar(CEREAL_NVP(description));
+  }
+  iOSEvent() = default;
+  iOSEvent(const std::string& d) : description(d) {}
 };
 
 CURRENT_EVENT(iOSAppLaunchEvent, iOSEvent) {
-    std::string binary_version;
-    std::string cf_version;
-    uint64_t app_install_time;
-    uint64_t app_update_time;
-    template<typename A> void serialize(A& ar) {
-        SUPER::serialize(ar);
-        ar(CEREAL_NVP(binary_version), CEREAL_NVP(cf_version), CEREAL_NVP(app_install_time), CEREAL_NVP(app_update_time));
-    }
-    iOSAppLaunchEvent() = default;
-    iOSAppLaunchEvent(const std::string& cf_version, uint64_t app_install_time, uint64_t app_update_time) : binary_version(__DATE__ " " __TIME__), cf_version(cf_version), app_install_time(app_install_time), app_update_time(app_update_time) {
-        description = "*FinishLaunchingWithOptions";
-    }
+  std::string binary_version;
+  std::string cf_version;
+  uint64_t app_install_time;
+  uint64_t app_update_time;
+  template <typename A>
+  void serialize(A & ar) {
+    SUPER::serialize(ar);
+    ar(CEREAL_NVP(binary_version),
+       CEREAL_NVP(cf_version),
+       CEREAL_NVP(app_install_time),
+       CEREAL_NVP(app_update_time));
+  }
+  iOSAppLaunchEvent() = default;
+  iOSAppLaunchEvent(const std::string& cf_version, uint64_t app_install_time, uint64_t app_update_time)
+      : binary_version(__DATE__ " " __TIME__),
+        cf_version(cf_version),
+        app_install_time(app_install_time),
+        app_update_time(app_update_time) {
+    description = "*FinishLaunchingWithOptions";
+  }
 };
 
 CURRENT_EVENT(iOSFocusEvent, iOSEvent) {
-    bool activated;  // True if gained focus, false if lost focus.
-    template<typename A> void serialize(A& ar) {
-        SUPER::serialize(ar);
-        ar(CEREAL_NVP(activated));
-    }
-    iOSFocusEvent() = default;
-    iOSFocusEvent(bool a, const std::string& d) {
-        description = d;
-        activated = a;
-    }
+  bool activated;  // True if gained focus, false if lost focus.
+  template <typename A>
+  void serialize(A & ar) {
+    SUPER::serialize(ar);
+    ar(CEREAL_NVP(activated));
+  }
+  iOSFocusEvent() = default;
+  iOSFocusEvent(bool a, const std::string& d) {
+    description = d;
+    activated = a;
+  }
 };
 
 @interface Midichlorians : NSObject
 
 // To be called in application:didFinishLaunchingWithOptions:
 // or in application:willFinishLaunchingWithOptions:
-+ (void)setup:(NSString *)serverUrl withLaunchOptions:(NSDictionary *)options;
++ (void)setup:(NSString*)serverUrl withLaunchOptions:(NSDictionary*)options;
 
 // Emits the event.
-+ (void)emit:(const MidichloriansEvent&) event;
++ (void)emit:(const MidichloriansEvent&)event;
 
 @end
 
--- ios/SampleClient/Unused_C5T/ios_client.h
+++ ios/SampleClient/Unused_C5T/ios_client.h
@@ -1,21 +1,21 @@
 /*******************************************************************************
  The MIT License (MIT)
- 
+
  Copyright (c) 2015:
- 
+
  * Dmitry "Dima" Korolev <dmitry.korolev@gmail.com>
  * Alexander Zolotarev <me@alex.bio> from Minsk, Belarus
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,7 +27,7 @@
 
 #ifndef C5T_IOS_CLIENT_H
 #define C5T_IOS_CLIENT_H
- 
+
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
@@ -39,22 +39,11 @@
 // or in application:willFinishLaunchingWithOptions:
 + (void)setup:(NSString *)serverUrl withLaunchOptions:(NSDictionary *)options;
 
-// Alternative to the previous setup method when not using automatic detection 
+// Alternative to the previous setup method when not using automatic detection
 // of whether the application is being launched for the first time.
-+ (void)setup:(NSString *)serverUrl andFirstLaunch:(BOOL)isFirstLaunch withLaunchOptions:(NSDictionary *)options;
-
-// Various logging methods.
-+ (void)logEvent:(NSString *)event;
-+ (void)logEvent:(NSString *)event withValue:(NSString *)value;
-+ (void)logEvent:(NSString *)event withKeyValueArray:(NSArray *)array;
-+ (void)logEvent:(NSString *)event withDictionary:(NSDictionary *)dictionary;
-
-#ifdef C5T_IOS_LOCATION_H
-+ (void)logEvent:(NSString *)event atLocation:(CLLocation *)location;
-+ (void)logEvent:(NSString *)event withValue:(NSString *)value atLocation:(CLLocation *)location;
-+ (void)logEvent:(NSString *)event withKeyValueArray:(NSArray *)array atLocation:(CLLocation *)location;
-+ (void)logEvent:(NSString *)event withDictionary:(NSDictionary *)dictionary atLocation:(CLLocation *)location;
-#endif  // #ifdef C5T_IOS_LOCATION_H
++ (void)setup:(NSString *)serverUrl
+       andFirstLaunch:(BOOL)isFirstLaunch
+       withLaunchOptions:(NSDictionary *)options;
 
 @end
 
